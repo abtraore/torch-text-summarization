7d60e66375b29c2166fc26cec7500ccc0d502498
atraore <abdrahtraore97@gmail.com>
2024-03-26 17:59:19 -0300

    Adding a configuration file

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+class TransformerConfig:
+    n_units = 256
+    n_heads = 2
+    n_blocks = 2
+    fully_connected_dim = 256
+    dropout_rate = 0.1
+    # training
+    epochs = 10
+    batch_size = 8
