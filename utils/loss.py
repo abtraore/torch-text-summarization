ea72e8f7359560fbfa8cfa88300b21c9ef4ab5c9
atraore <abdrahtraore97@gmail.com>
2024-03-27 19:16:10 -0300

    Adding comments

--- utils/loss.py
+++ utils/loss.py
@@ -4,12 +4,17 @@ import torch.nn.functional as F
 
 def mask_loss(y_pred, y_true):
 
-    y_pred = torch.flatten(y_pred, 0, 1)
-    y_true = torch.flatten(y_true, 0, 1)
+    # Vectorize predictions and ground truth.
+    y_pred = torch.flatten(y_pred, 0, 1)  # (B, S, vocab_size) -> (B*S, vocab_size)
+    y_true = torch.flatten(y_true, 0, 1)  # (B, S) -> (B*S, )
 
+    # Create a mask to avoid adding 0 (padding to the loss computation).
     mask_true = (y_true != 0).float()
-    loss = F.cross_entropy(y_pred, y_true, reduction="none")
-    loss *= mask_true
-    loss = torch.sum(loss) / torch.sum(mask_true)
+
+    loss = F.cross_entropy(y_pred, y_true, reduction="none")  # Compute the loss
+    loss *= mask_true  # Turn padding values to 0.
+    loss = torch.sum(loss) / torch.sum(
+        mask_true
+    )  # Compute the loss on values that are padding.
 
     return loss
