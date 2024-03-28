408431bed6e44a5d88516a47cd0aeec23ac3ab07
atraore <abdrahtraore97@gmail.com>
2024-03-28 11:09:07 -0300

    Update configs

--- utils/config.py
+++ utils/config.py
@@ -1,13 +1,15 @@
 class TransformerConfig:
-    n_blocks = 5
+    # Transformer
+    n_blocks = 2
     n_heads = 2
-    d_model = 512  # Embedding size
-    fully_connected_dim = 2048
+    d_model = 128  # Embedding size
+    fully_connected_dim = 128
     max_position_encoding = 256
-    max_seq_length_input = 150  # TODO: Add to model
-    max_seq_length_target = 50  # TODO: Add to model
+    max_seq_length_input = 150
+    max_seq_length_target = 50
     dropout_rate = 0.1
-    # training
-    epochs = 14
+
+    # Training
+    epochs = 30
     batch_size = 64
     weights_path = "summarizer.pt"
